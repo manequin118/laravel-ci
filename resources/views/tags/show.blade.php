504798125c2e825bc6e77e81d028038c029fb59d
manequin118 <manequin118@gmail.com>
2021-11-12 21:45:06 +0900

    test

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -3,18 +3,18 @@
 @section('title', $tag->hashtag)
 
 @section('content')
-  @include('nav')
-  <div class="container">
-    <div class="card mt-3">
-      <div class="card-body">
-        <h2 class="h4 card-title m-0">{{ $tag->hashtag }}</h2>
-        <div class="card-text text-right">
-          {{ $tag->articles->count() }}件
-        </div>
+@include('nav')
+<div class="container">
+  <div class="card mt-3">
+    <div class="card-body">
+      <h2 class="h4 card-title m-0">{{ $tag->hashtag }}</h2>
+      <div class="card-text text-right">
+        {{ $tag->articles->count() }}件
       </div>
     </div>
-    @foreach($tag->articles as $article)
-      @include('articles.card')
-    @endforeach
   </div>
+  @foreach($tag->articles as $article)
+  @include('articles.card')
+  @endforeach
+</div>
 @endsection
